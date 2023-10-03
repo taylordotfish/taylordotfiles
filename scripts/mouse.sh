dcd42289ae946f125f7d1ce6be0dbfa417fa5b4a
taylor.fish <contact@taylor.fish>
2023-10-03 05:32:07 -0700

    Fix shell errors

--- scripts/mouse.sh
+++ scripts/mouse.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eu
 pointer="1 2 3"
-[ -n "$LEFT_MOUSE" ] && pointer=$(rev "$pointer")
+[ -n "${LEFT_MOUSE-}" ] && pointer=$(rev "$pointer")
 xmodmap -e "pointer = $pointer" > /dev/null 2>&1
