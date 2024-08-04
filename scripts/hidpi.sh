4fd7b027cd7f8caaa3ed7c6bfa73479d85b67b77
taylor.fish <contact@taylor.fish>
2024-08-04 13:30:04 -0700

    Improve hidpi support

--- scripts/hidpi.sh
+++ scripts/hidpi.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 set -euf
-if [ -n "${DISPLAY-}" ]; then
-    dpi=$(xrdb -query | grep '^Xft\.dpi:' | cut -f2)
-else
-    dpi=192
-fi
 export HIDPI=1
 export GDK_SCALE=2
-export GDK_DPI_SCALE=$(printf '%.2g' "$(echo "scale=3;96/$dpi" | bc)")
 export QT_SCALE_FACTOR=2
-export QT_FONT_DPI=96
 exec "$@"
