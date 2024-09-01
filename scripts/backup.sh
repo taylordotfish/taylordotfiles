09f14d4987f97be796ab0927172048988d90614f
taylor.fish <contact@taylor.fish>
2024-08-31 21:27:43 -0700

    Improve scripts/backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-set -euf
+set -uf
 sudo rsync -avF --delete-after --delete-excluded --info=progress2 / \
     /mnt/backup/
+status=$?
+set -e
+if [ "$status" -ne 0 ]; then
+    printf >&2 "%s\n" 'warning: `sudo rsync` exited with status '"$status"
+fi
