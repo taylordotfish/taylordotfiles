cd5224e08e0154506c4225e91b8c6343223710f8
taylor.fish <contact@taylor.fish>
2025-01-05 20:21:26 -0800

    Update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-set -uf
+set -euf
+if ! [ -d /mnt/backup ]; then
+    printf >&2 '%s\n' "error: /mnt/backup does not exist"
+    exit 1
+fi
+set +e
 sudo rsync -avF --delete-after --delete-excluded --info=progress2 / \
     /mnt/backup/
 status=$?
 set -e
 if [ "$status" -ne 0 ]; then
-    printf >&2 "%s\n" 'warning: `sudo rsync` exited with status '"$status"
+    printf >&2 '%s\n' 'warning: `sudo rsync` exited with status '"$status"
 fi
