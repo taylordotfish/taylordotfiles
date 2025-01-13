5f09d0a1cba78aa7c7c23f89f4c21375621f8039
taylor.fish <contact@taylor.fish>
2025-01-12 22:10:43 -0800

    Update generate.sh scripts

--- .config/dunst/generate.sh
+++ .config/dunst/generate.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -euf
 cd "$(dirname "$0")"
-echo '# AUTOMATICALLY GENERATED!' > dunstrc
-echo '# CHANGES WILL BE LOST!' >> dunstrc
-echo >> dunstrc
-m4 dunstrc.m4 >> dunstrc
+{
+    cat << 'EOF'
+# AUTOMATICALLY GENERATED!
+# CHANGES WILL BE LOST!
+
+EOF
+    m4 dunstrc.m4
+} > dunstrc
--- .config/i3/generate.sh
+++ .config/i3/generate.sh
@@ -2,18 +2,20 @@
 set -euf
 cd "$(dirname "$0")"
 {
-    cat << \EOF
+    cat << 'EOF'
 # AUTOMATICALLY GENERATED!
 # CHANGES WILL BE LOST!
+
 EOF
     m4 config.m4
 } > config
 
 cd
 {
-    cat << \EOF
+    cat << 'EOF'
 # AUTOMATICALLY GENERATED!
 # CHANGES WILL BE LOST!
+
 EOF
     m4 .i3status.conf.m4
 } > .i3status.conf
