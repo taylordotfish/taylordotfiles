7f75b63ebf9dbcf6fa2747d4133f322cf2405ac6
taylor.fish <contact@taylor.fish>
2025-05-22 11:57:50 -0700

    x11vnc.sh: print nonzero status

--- scripts/x11vnc.sh
+++ scripts/x11vnc.sh
@@ -8,6 +8,10 @@ while true; do
         -display "${DISPLAY:-:0}" \
         -noxdamage \
         "$@" && true
+    status=$?
+    if [ "$status" -ne 0 ]; then
+        printf >&2 '%s\n' "x11vnc exited with status $status"
+    fi
     printf >&2 '%s\n' "Restarting..."
     sleep 4
 done
