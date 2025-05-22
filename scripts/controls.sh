5822393009c5c04cf6febc496cf76e27d84d324b
taylor.fish <contact@taylor.fish>
2025-05-22 11:57:09 -0700

    Update controls.sh

--- scripts/controls.sh
+++ scripts/controls.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -euf
-cd "$(dirname "$0")"
-[ -x keyboard.sh ] && ./keyboard.sh
-[ -x mouse.sh ] && ./mouse.sh
+for script in keyboard.sh mouse.sh; do
+    script=$(dirname "$0")/$script
+    if [ -x "$script" ]; then
+        "$script"
+    fi
+done
