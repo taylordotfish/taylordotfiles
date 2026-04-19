6b1e54f2fa1551ae61a575b51dfe2b51492b3c0f
taylor.fish <contact@taylor.fish>
2026-04-19 01:31:28 -0700

    Update .config/i3/rename-workspace.sh
    Miscellaneous improvements, better escaping.

--- .config/i3/rename-workspace.sh
+++ .config/i3/rename-workspace.sh
@@ -1,21 +1,25 @@
-# Copyright (C) 2025 taylor.fish <contact@taylor.fish>
+# Copyright (C) 2025-2026 taylor.fish <contact@taylor.fish>
 # License: GNU GPL version 3 or later
 set -euf
 
-escape() {
-    printf '%s\n' "$1" | sed 's/\\/\\\\/g;s/"/\\"/g'
+escape_sh() {
+    printf '%s\n' "$1" | sed "s/'/'\\\\''/g"
+}
+
+escape_i3() {
+    printf '%s\n' "$1" | sed 's/[\\"]/\\&/g'
 }
 
 prompt() {
-    exec i3-input -F "exec --no-startup-id sh \
-\"$(escape "$0")\" rename \"%s\"" -P "rename workspace: " "$@"
+    exec i3-input -F "exec --no-startup-id sh '$(escape_sh "$0")' \
+rename \"%s\"" -P "rename workspace: " "$@"
 }
 
 rename() {
     #exec >> "$(dirname "$0")"/rename-workspace.log 2>&1
-    local new_name=$1
+    local new_name="$1"
     [ -n "$new_name" ] || return
-    local new_number=${new_name%%:*}
+    local new_number="${new_name%%:*}"
     case "$new_number" in
         *[!0-9]*) new_number= ;;
     esac
@@ -26,6 +30,7 @@ rename() {
     else
         new_number=$(i3-msg -t get_workspaces |
             jq -r 'map(select(.focused).name)[0] // empty' |
+            head -n1 |
             sed 's/^\([0-9]*\).*/\1/')
         new_label=$new_name
     fi
@@ -33,9 +38,9 @@ rename() {
     if [ -n "$new_number" ]; then
         final_name="$new_number: $new_label"
     else
-        final_name="$new_label"
+        final_name=$new_label
     fi
-    exec i3-msg "rename workspace to \"$(escape "$final_name")\""
+    exec i3-msg "rename workspace to \"$(escape_i3 "$final_name")\""
 }
 
 if [ "$#" -eq 0 ]; then
