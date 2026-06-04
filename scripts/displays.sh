3034ffab5b8e9e37daf5d0abf0a3f2870749c850
taylor.fish <contact@taylor.fish>
2026-06-04 01:10:54 -0700

    Update displays.sh, .xsession

--- scripts/displays.sh
+++ scripts/displays.sh
@@ -6,6 +6,17 @@ set -euf
 dir=$(dirname "$0")
 wm=$(xprop -root 8s '\n$0\n' XSESSION_TARGET | tail -n+2 | tr -dc A-Za-z0-9_-)
 
+if [ -f "$dir"/init-monitors.sh ]; then
+    # Calls to xrandr go in this file
+    . "$dir"/init-monitors.sh
+fi
+
+run_if_exists() {
+    if [ -x "$1" ]; then
+        "$@"
+    fi
+}
+
 monitors_newer() {
     [ ~/.cache/monitor-utils/monitors.json -nt "$1" ]
 }
@@ -26,28 +37,31 @@ make_xsettingsd() {
     } > ~/.xsettingsd
 }
 
-if [ -f "$dir"/init-monitors.sh ]; then
-    # Calls to xrandr go in this file
-    . "$dir"/init-monitors.sh
+run_if_exists ~/.config/monitor-utils/generate.sh
+eval "$(~/scripts/monitor-utils/sh/globals.sh)"
+xrdb ~/.Xresources.m4 -cpp m4
+
+if command -v dunst > /dev/null; then
+    run_if_exists ~/.config/dunst/generate.sh
+    systemctl --user restart dunst.service
 fi
 
-eval "$(~/scripts/monitor-utils/sh/globals.sh)"
-
 if [ "$wm" = i3 ]; then
+    run_if_exists ~/.config/i3/generate.sh
     if pgrep -x i3 > /dev/null && monitors_newer ~/.config/i3/config.m4; then
-        ~/.config/i3/generate.sh
         i3-msg -q reload
     fi
 
-    if [ -x ~/.fehbg ] && command -v feh > /dev/null; then
-        ~/.fehbg
-    elif [ "$global_monitor_tech" = epaper ]; then
+    if [ "$global_monitor_tech" = epaper ]; then
         setroot -solid '#ffffff'
     else
         setroot -solid '#000000'
     fi
+    if ! run_if_exists ~/.fehbg; then
+        printf >&2 '%s\n' "warning: failed to run .fehbg"
+    fi
 
-    if ! pgrep -x xsettingsd; then
+    if ! pgrep -x xsettingsd > /dev/null; then
         make_xsettingsd
         xsettingsd &
     elif monitors_newer ~/.xsettingsd.m4; then
@@ -58,10 +72,7 @@ if [ "$wm" = i3 ]; then
     if { command -v picom && ! pgrep -x picom; } > /dev/null; then
         picom --daemon
     fi
-
-    if command -v profilectl > /dev/null &&
-        cat ~/.config/color.jcnf 2> /dev/null | grep -q .
-    then
+    if [ -f ~/.config/color.jcnf ] && command -v profilectl > /dev/null; then
         profilectl load
     fi
 fi
